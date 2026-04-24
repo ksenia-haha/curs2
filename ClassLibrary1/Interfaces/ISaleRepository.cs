041c240c87e3c7f277580fcc839c1604093a4426
ksenia-haha <ksenia2006r@gmail.com>
2026-04-24 18:04:44 +0300

    ISaleRepository

--- ClassLibrary1/Interfaces/ISaleRepository.cs
+++ ClassLibrary1/Interfaces/ISaleRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore.Storage;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,5 +11,6 @@ namespace Domain.Interfaces
     {
         Task<IEnumerable<Sale>> GetByClientId(int id);
         Task<IEnumerable<Sale>> GetByEmployeeId(int id);
+        Task<IDbContextTransaction> BeginTransactionAsync();
     }
 }
