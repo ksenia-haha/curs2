755e5d373e6a629412f57696d1a892f1e7505ee9
ksenia-haha <ksenia2006r@gmail.com>
2026-04-27 19:04:23 +0300

    Update int? Id

--- ClassLibrary1/Entities/Employee.cs
+++ ClassLibrary1/Entities/Employee.cs
@@ -9,7 +9,7 @@ namespace Domain
 {
     public class Employee
     {
-        public int? Id { get; set; }
+        public int Id { get; set; }
         public string Surname { get; set; }
         public string Name { get; set; }
         public string Patronymic { get; set; }
