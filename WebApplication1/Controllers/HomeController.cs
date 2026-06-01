b739fd01acfd44995e237b99997e29edd09b6fd3
ksenia <ksenia2006r@gmail.com>
2026-06-01 21:03:28 +0300

    Аналитика

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 using System.Diagnostics;
+using Domain.Interfaces;
+using Infrastructure.Repositories;
 using Microsoft.AspNetCore.Mvc;
 using WebApplication1.Models;
 
@@ -6,8 +8,42 @@ namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly ISaleRepository _saleRepository;
+        private readonly IExemplarRepository _exemplarRepository;
+        private readonly IEditionRepository _editionRepository;
+        private readonly IReturnRepository _returnRepository;
+
+        public HomeController(IExemplarRepository exemplarRepository, ISaleRepository saleRepository, IEditionRepository editionRepository, IReturnRepository returnRepository)
         {
+            _exemplarRepository = exemplarRepository;
+            _saleRepository = saleRepository;
+            _editionRepository = editionRepository;
+            _returnRepository = returnRepository;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var sales = await _saleRepository.GetAllAsync();
+            var editions = await _editionRepository.GetAllAsync();
+            var exemplars = await _exemplarRepository.GetAvailableExemplarsAsync();
+            var currentDate = DateTime.Now;
+
+            var monthSales = sales
+                .Where(s => s.Date.Year == currentDate.Year && s.Date.Month == currentDate.Month)
+                .ToList();
+
+            var monthSalesCount = monthSales.Count();
+            var monthRevenue = monthSales.Sum(s => s.Sum);
+
+            var editionCount = editions.Count();
+            var exemplarCount = exemplars.Count();
+
+            ViewBag.MonthSales = monthSalesCount;
+            ViewBag.MonthRevenue = monthRevenue;
+            ViewBag.EditionsCount = editionCount;
+            ViewBag.ExemplarCount = exemplarCount;
+
+
             return View();
         }
 
